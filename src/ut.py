88cdb596fe4e992a1127f0da27f73adc3c1d8027
Stefano Pedroni <s.pedroni@rulex.ai>
2020-12-14 14:10:19 +0100

    [DEV]: add comments to envOverride utility

--- src/ut.py
+++ src/ut.py
@@ -1,9 +1,13 @@
 import os
 
-def envOverride(settings, path=""):
+def envOverride(settings, path="", seprator="_"):
+    '''
+    Override dict values with environment variables sharing the same name of the key-path,
+    separated by the **separator** value
+    '''
     for k in settings:
         if type(settings[k]) is dict:
-            settings[k] = envOverride(settings[k], path=k+"_")
+            settings[k] = envOverride(settings[k], path=k+seprator)
         else:
             settings[k] = os.environ.get((path+k).upper(), settings[k])
     return settings
